a79a87d07505492178322b279ca0eae4712aef27
joonsol <71648638+joonsol@users.noreply.github.com>
2025-06-05 16:53:28 +0900

    1

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,18 @@
-const express = require("express");
-const app =express();
+require("dotenv").config()
+const express = require("express")
+const mongoose = require("mongoose")
+const app = express()
 const PORT = 3000;
 
-app.get("/", (req,res)=>{
-    res.send("hello world")
+app.get('/', (req, res) => {
+  res.send("Hello World!")
 })
 
-app.listen(PORT,()=>{
-    console.log("server is running")
+mongoose.connect(process.env.MONGO_URI).then(() => {
+  console.log("mongoDB 연결")
+}).catch((error) => console.log("DB와 연결 실패", error))
+
+
+app.listen(PORT, () => {
+  console.log("Server is running!")
 })
