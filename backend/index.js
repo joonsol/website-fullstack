c4604d4e087fb43c82203099ec62a55a8138f259
joonsol <71648638+joonsol@users.noreply.github.com>
2025-06-09 17:20:35 +0900

    .

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,15 @@ const express = require("express")
 const mongoose = require("mongoose")
 const cookieParser=require("cookie-parser")
 const app = express()
+const cors=require("cors")
 const PORT = 3000;
 
 const userRoutes =require("./routes/user")
 
-
+app.use(cors({
+  origin:"http://localhost:5173",
+  credentials:true
+}))
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser())
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,62 +1,132 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
-import { createBrowserRouter, RouterProvider,Outlet } from "react-router-dom"
+import { createBrowserRouter, RouterProvider, Outlet, Navigate } from "react-router-dom"
 import Navbar from './Components/Navbar/Navbar'
-import Footer from './Components/Navbar/Footer/Footer'
+import Footer from './Components/Footer/Footer'
 import MainPage from './Page/MainPage/MainPage'
 import Leadership from './Page/Leadership/Leadership'
 import Board from './Page/Board/Board'
 import Service from './Page/Service/Service'
 import Contact from './Page/Contact/Contact'
 import About from './Page/About/About'
+
 import AdminLogin from './Page/Admin/AdminLogin'
-function Layout(){
-  return(
+import AdminPosts from './Page/Admin/AdminPosts'
+import AdminEditPost from "./Page/Admin/AdminEditPost";
+import AdminCreatePost from "./Page/Admin/AdminCreatePost";
+import AdminContacts from "./Page/Admin/AdminContacts";
+import AdminNavbar from './Components/AdminNavbar/AdminNavbar'
+
+import axios from "axios"
+
+function AuthRedirectRoute() {
+  const [isAuthenticcated, setIsAuthenticated] = useState(null)
+
+
+  useEffect(() => {
+    const verifytoken = async () => {
+
+      try {
+        const response = await axios.post("http://localhost:3000/api/auth/verify-token", {}, {
+          withCredentials: true
+        })
+      } catch (error) {
+        console.log("토큰 인증 실패:", error)
+        setIsAuthenticated(false)
+      }
+    }
+    verifytoken()
+  }, [])
+
+  if (isAuthenticcated === null) {
+    return null;
+  }
+  return isAuthenticcated ? <Navigate to="/admin/posts" replace /> : <Outlet />
+}
+
+
+function Layout() {
+  return (
     <>
-    <Navbar/>
-    <Outlet/>
-    <Footer/>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </>
+  )
+}
+function AdminLayout() {
+  return (
+    <>
+      <AdminNavbar />
+      <Outlet />
+
     </>
   )
 }
 const router = createBrowserRouter([
   {
-    path:"/",
-    element:<Layout/>,
-    children:[
+    path: "/",
+    element: <Layout />,
+    children: [
       {
-        index:true,
-        element:<MainPage/>
+        index: true,
+        element: <MainPage />
       },
       {
-        path:"/about",
-        element:<About/>
+        path: "/about",
+        element: <About />
       },
       {
-        path:"/leadership",
-        element:<Leadership/>
+        path: "/leadership",
+        element: <Leadership />
       },
       {
-        path:"/board",
-        element:<Board/>
+        path: "/board",
+        element: <Board />
       },
       {
-        path:"/our-services",
-        element:<Service/>
+        path: "/our-services",
+        element: <Service />
       },
       {
-        path:"/contact",
-        element:<Contact/>
+        path: "/contact",
+        element: <Contact />
       },
     ]
-  },{
-    path:"/admin",
-    element:<AdminLogin/>
+  }, {
+    path: "/admin",
+    element: <AuthRedirectRoute />,
+    children: [{
+      index: true,
+      element: <AdminLogin />
+    }]
+  },
+  {
+    path: "/admin",
+    element: <AdminLayout />,
+    children: [
+      {
+        path: "posts",
+        element: <AdminPosts />
+      },
+      {
+        path: "create-post",
+        element: <AdminCreatePost />
+      },
+      {
+        path: "edit-posts/:id",
+        element: <AdminEditPost />
+      },
+      {
+        path: "contacts",
+        element: <AdminContacts />
+      },
+    ]
   }
 ])
 function App() {
 
-  return <RouterProvider router={router}/>
+  return <RouterProvider router={router} />
 }
 
 export default App
